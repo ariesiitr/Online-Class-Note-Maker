cb32621abcecf1edfecd8ba1237457e6647c39e4
Shreyansh Saxena <58340822+shrey-001@users.noreply.github.com>
2021-05-09 19:44:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 import pdfplumber
 
-import functions as fun
+import summerizer as sum
 import caption as cap
 import PyPDF2
 import fitz
 import io
 from PIL import Image
-
-pdfFileObj = open('2.pdf', 'rb')
-
-
+import warnings
+warnings.filterwarnings("ignore")
+file = '2.pdf'
+pdfFileObj = open(file, 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-file = "2.pdf"
+
 # open the file
 pdf_file = fitz.open(file)
 
+#textfile
+
+
 x = (pdfReader.numPages)
-with pdfplumber.open(r'2.pdf') as pdf:
+with pdfplumber.open(file) as pdf:
     for i in range(0,x):
         first_page = pdf.pages[i]
-        print('**********************************')
-        if None:
-            print("Page No.",i+1)
-            print("")
-        else:
-            print("Page No.",i+1)
-            print("")
-        fun.summerize(first_page.extract_text())
+        text,summ=sum.summerize(first_page.extract_text())
         page = pdf_file[i]
         image_list = page.getImageList()
         # printing number of images found in this page
-        
+        captions=[]
         for image_index, img in enumerate(page.getImageList(), start=1):
             # get the XREF of the image
             xref = img[0]
@@ -42,12 +38,14 @@ with pdfplumber.open(r'2.pdf') as pdf:
             # load it to PIL
             im = Image.open(io.BytesIO(image_bytes))
             im = cap.encodeImage(im).reshape((1,2048))
+            captions.append(cap.generateCaption(im))
             
-            print("Caption ", image_index,":",cap.generateCaption(im))
-            
-            #print("---------------------")
-            #print('**********************************')
-            #im.show()
-            # save it to local disk
-            #im.save(open(f"/home/shreyansh/repo/Image-Caption/Images_extracted/image{i + 1}_{image_index}.{image_ext}", "wb"))
-
+        print("*************************")
+        print("Page No. ", i+1)
+        print("Text :")
+        print(text)
+        print("Summary :")
+        print(summ)
+        for j in captions:
+         print("Caption :", j)
+        print("*************************")
